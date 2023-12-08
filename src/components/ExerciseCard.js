839271605387c5c21bdc18cf3b6e35d675da6faa
yzanaty <zanaty.yassine@outlook.com>
2023-12-08 15:32:27 +0100

    feature add ExerciseCard.js

---
+++ src/components/ExerciseCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+
+const ExerciseCard = ({ exercise }) => {
+  return (
+    <Link to={`/exercise/${exercise.id}`} className="exercise-card">
+      <img src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <Stack direction="row">
+        <Button
+          sx={{
+            ml: "21px",
+            color: "white",
+            background: "#ffa9a9",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.bodyPart}
+        </Button>
+        <Button
+          sx={{
+            ml: "21px",
+            color: "white",
+            background: "#fcc757",
+            fontSize: "14px",
+            borderRadius: "20px",
+            textTransform: "capitalize",
+          }}
+        >
+          {exercise.target}
+        </Button>
+      </Stack>
+      <Typography
+        sx={{
+          ml: "21px",
+          color: "#000",
+          fontWeight: "bold",
+          mt: "11px",
+          pb: "10px",
+          textTransform: "capitalize",
+          fontSize: "22px",
+        }}
+      >
+        {exercise.name}
+      </Typography>
+    </Link>
+  );
+};
+
+export default ExerciseCard;
